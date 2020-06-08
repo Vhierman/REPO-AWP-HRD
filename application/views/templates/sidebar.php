6f20fe5ce79798a16aad627539d71ce810d22883
Vhierman <achmadfirmansyah989@yahoo.co.id>
2020-06-08 14:13:13 +0700

    Update Warna Sidebar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,58 +1,58 @@
 <!-- Sidebar -->
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav bg-gray-900 sidebar sidebar-dark accordion" id="accordionSidebar">
 
-    <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fab fa-autoprefixer"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">WPP Admin </div>
-    </a>
+	<!-- Sidebar - Brand -->
+	<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+		<div class="sidebar-brand-icon rotate-n-15">
+			<i class="fab fa-autoprefixer"></i>
+		</div>
+		<div class="sidebar-brand-text mx-3">WPP Admin </div>
+	</a>
 
-    <!-- Divider / Garis Pembatas -->
-    <hr class="sidebar-divider">
+	<!-- Divider / Garis Pembatas -->
+	<hr class="sidebar-divider">
 
-    <!-- Query Relasi Menu -->
-    <?php
-    $role_id = $this->session->userdata('role_id');
-    $queryMenu = "SELECT `user_menu`.`id`,`menu` 
+	<!-- Query Relasi Menu -->
+	<?php
+	$role_id = $this->session->userdata('role_id');
+	$queryMenu = "SELECT `user_menu`.`id`,`menu` 
                     FROM `user_menu` JOIN `user_access_menu`
                     ON  `user_menu`.`id` = `user_access_menu`.`menu_id`
         WHERE `user_access_menu`.`role_id` = $role_id
         ORDER BY `user_access_menu`.`menu_id` ASC ";
-    $menu = $this->db->query($queryMenu)->result_array();
+	$menu = $this->db->query($queryMenu)->result_array();
 
-    ?>
+	?>
 
-    <?php foreach ($menu as $n) : ?>
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#<?= $n['menu']; ?>" aria-expanded="true" aria-controls="collapseTwo">
-                <i class="fas fa-fw fa-folder"></i>
-                <span><?= $n['menu']; ?></span>
-            </a>
-            <div id="<?= $n['menu']; ?>" class="collapse" aria-labelledby="<?= $n['menu']; ?>" data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <?php
-                        $menuID = $n['id'];
-                        $querySubMenu = "SELECT * 
+	<?php foreach ($menu as $n) : ?>
+		<li class="nav-item">
+			<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#<?= $n['menu']; ?>" aria-expanded="true" aria-controls="collapseTwo">
+				<i class="fas fa-fw fa-folder"></i>
+				<span><?= $n['menu']; ?></span>
+			</a>
+			<div id="<?= $n['menu']; ?>" class="collapse" aria-labelledby="<?= $n['menu']; ?>" data-parent="#accordionSidebar">
+				<div class="bg-white py-2 collapse-inner rounded">
+					<?php
+					$menuID = $n['id'];
+					$querySubMenu = "SELECT * 
                                     FROM `user_sub_menu` 
                                     WHERE `menu_id` = $menuID 
                                     AND  `is_active` = 1 ";
-                        $subMenus = $this->db->query($querySubMenu)->result_array();
-                        ?>
-                    <?php foreach ($subMenus as $sms) : ?>
+					$subMenus = $this->db->query($querySubMenu)->result_array();
+					?>
+					<?php foreach ($subMenus as $sms) : ?>
 
-                        <a class="collapse-item" href="<?= base_url($sms['url']); ?> "><i class="<?= $sms['icon']; ?>"></i>
-                            <span><?= $sms['title']; ?></span></a>
-                    <?php endforeach; ?>
-                </div>
-            </div>
-        </li>
-        <hr class="sidebar-divider my-0">
-    <?php endforeach; ?>
+						<a class="collapse-item" href="<?= base_url($sms['url']); ?> "><i class="<?= $sms['icon']; ?>"></i>
+							<span><?= $sms['title']; ?></span></a>
+					<?php endforeach; ?>
+				</div>
+			</div>
+		</li>
+		<hr class="sidebar-divider my-0">
+	<?php endforeach; ?>
 
-    <?php
-    /* Tampilan Menu Dalam Bentuk Lain
+	<?php
+	/* Tampilan Menu Dalam Bentuk Lain
     <!-- Looping Menu -->
     <?php foreach ($menu as $m) : ?>
     <div class="sidebar-heading">
@@ -83,23 +83,23 @@
     <hr class="sidebar-divider my-0 mt-2">
     <?php endforeach; ?>
     */
-    ?>
+	?>
 
-    <!-- Logout -->
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('auth/logout'); ?>">
-            <i class="fas fa-fw fa-sign-out-alt"></i>
-            <span>Logout</span></a>
-    </li>
-    <!-- End of Logout -->
+	<!-- Logout -->
+	<li class="nav-item">
+		<a class="nav-link" href="<?= base_url('auth/logout'); ?>">
+			<i class="fas fa-fw fa-sign-out-alt"></i>
+			<span>Logout</span></a>
+	</li>
+	<!-- End of Logout -->
 
-    <!-- Divider / Garis Pembatas -->
-    <hr class="sidebar-divider my-0">
+	<!-- Divider / Garis Pembatas -->
+	<hr class="sidebar-divider my-0">
 
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
+	<!-- Sidebar Toggler (Sidebar) -->
+	<div class="text-center d-none d-md-inline">
+		<button class="rounded-circle border-0" id="sidebarToggle"></button>
+	</div>
 
 </ul>
 <!-- End of Sidebar -->
